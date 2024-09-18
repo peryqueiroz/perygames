bd8602b7bb402995e676e2332683c561c8266ff3
Pery <pery.vieira@gmail.com>
2024-09-17 23:51:10 -0300

    first commit

---
+++ perygames/src/main/kotlin/com/b1thouse/perygames/PerygamesApplication.kt
@@ -0,0 +1,11 @@
+package com.b1thouse.perygames
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PerygamesApplication
+
+fun main(args: Array<String>) {
+	runApplication<PerygamesApplication>(*args)
+}
---
+++ perygames/src/test/kotlin/com/b1thouse/perygames/PerygamesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.b1thouse.perygames
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PerygamesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
