c303bf6a0fd0e9fc3ed6e4e37cf17645dc36ee11
Pery Vieira <pery.vieira@gmail.com>
2025-10-07 15:41:22 -0300

    sec config

---
+++ src/main/kotlin/com/b1thouse/perygames/application/config/SecurityConfig.kt
@@ -0,0 +1,54 @@
+package com.b1thouse.perygames.application.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig() {
+
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity
+            .cors { }
+            .csrf { csrf -> csrf.disable() }
+            .sessionManagement { session -> session.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS
+            )
+            }
+            .authorizeHttpRequests{ authorize -> authorize
+                .anyRequest().permitAll()
+            }
+            .build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration().apply {
+            allowedOrigins = listOf("*")
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+        }
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+}
